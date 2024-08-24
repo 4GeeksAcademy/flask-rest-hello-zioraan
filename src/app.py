bf03b01dab41150ba12be7657119cfbc5e91b9a9
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-08-24 00:08:19 +0000

    final

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -32,17 +32,73 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route('/', methods=['POST'])
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def handle_getting_people():
+#get ALL people
+    people = Person.query.all()
+    person_dictionaries = []
+    for person in people:
+        person_dictionaries.append(
+            person.serialize()
+        )
+    return jsonify(person_dictionaries), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_getting_a_person(id):
+#get a single person
+    person_info = Person.query.get({"id": id})
+    person_dictionary = []
+    for person in person_info:
+        person_dictionary.append(
+            person.serialize()
+        )
+    
+    return jsonify(person_dictionary), 200
 
+@app.route('/planets', methods=['GET'])
+def handle_getting_planets():
+#get ALL planets
+    planets = Planet.query.all()
+    planet_dictionaries = []
+    for planet in planets:
+        planet_dictionaries.append(
+            planet.serialize()
+        )
+    return jsonify(planet_dictionaries), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_getting_a_planet(id):
+#get a single planet
+    planet_info = Person.query.get({"id": id})
+    planet_dictionary = []
+    for planet in planet_info:
+        planet_dictionary.append(
+        planet.serialize()
+    )
+    return jsonify(planet_dictionary), 200
+
+@app.route('/users', methods=['GET'])
+def handle_getting_users():
+#get ALL users
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def handle_getting_current_favorites():
+#get ALL favorites of user
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST', 'DELETE'])
+def handle_adding_favorite_planet():
+#add planet to current user's favorites
+    return jsonify(response_body), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST', 'DELETE'])
+def handle_adding_favorite_people():
+#add planet to current user's favorites
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +17,53 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.String(20))
+    skin_color = db.Column(db.String(20))
+    birth_year = db.Column(db.String(20))
+    eye_color = db.Column(db.String(20))
+    gender = db.Column(db.String(20))
+    homeworld = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer)
+    gravity = db.Column(db.String(40))
+    climate = db.Column(db.String(40))
+    terrain = db.Column(db.String(40))
+    population = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    person = db.Column(db.Integer, db.ForeignKey('person.id'))
+    planet = db.Column(db.Integer, db.ForeignKey('planet.id'))
+
+    
